90fb5622d2eaa1bde1416d6780487d3da465c418
Eric Liu <yuanliu325@gmail.com>
2023-02-24 22:27:38 +0800

    fix bug in vex

--- vextract/vocal_extract.py
+++ vextract/vocal_extract.py
@@ -15,25 +15,25 @@ class VEX(object):
         audio = (audio / np.iinfo(audio.dtype).max).astype(np.float32)
         if len(audio.shape) > 1:
             audio = librosa.to_mono(audio.transpose(1, 0))
-        if sampling_rate != 16000:
-            audio = librosa.resample(audio, orig_sr=sampling_rate, target_sr=16000)
+        # if sampling_rate != 16000:
+        #     audio = librosa.resample(audio, orig_sr=sampling_rate, target_sr=16000)
 
         sample_tmp_wav_filename = "samtmpwav.wav"
-        soundfile.write(sample_tmp_wav_filename, audio, 16000, format="wav")
+        soundfile.write(sample_tmp_wav_filename, audio, sampling_rate, format="wav")
 
-        os.system(f"spleeter separate -p spleeter:2stems-16kHz -o output {sample_tmp_wav_filename}")
+        os.system(f"spleeter separate -p spleeter:2stems -o output {sample_tmp_wav_filename}")
 
         vocal_file = f"output/samtmpwav/vocals.wav"
         accompaniment_file = f"output/samtmpwav/accompaniment.wav"
 
         vocal_rate, vocal_audio = read(vocal_file)
-        vocal_audio = (vocal_audio / np.iinfo(audio.dtype).max).astype(np.float32)
-        vocal_audio = librosa.resample(vocal_audio, orig_sr=vocal_rate, target_sr=sampling_rate)
-        vocal_audio = (np.array(vocal_audio) * 32768).astype('int16')
+        # vocal_audio = (vocal_audio / np.iinfo(audio.dtype).max).astype(np.float32)
+        # vocal_audio = librosa.resample(vocal_audio, orig_sr=vocal_rate, target_sr=sampling_rate)
+        # vocal_audio = (np.array(vocal_audio) * 32768).astype('int16')
         accompaniment_rate, accompaniment_audio = read(accompaniment_file)
-        accompaniment_audio = (accompaniment_audio / np.iinfo(audio.dtype).max).astype(np.float32)
-        accompaniment_audio = librosa.resample(accompaniment_audio, orig_sr=accompaniment_rate, target_sr=sampling_rate)
-        accompaniment_audio = (np.array(accompaniment_audio) * 32768).astype('int16')
+        # accompaniment_audio = (accompaniment_audio / np.iinfo(audio.dtype).max).astype(np.float32)
+        # accompaniment_audio = librosa.resample(accompaniment_audio, orig_sr=accompaniment_rate, target_sr=sampling_rate)
+        # accompaniment_audio = (np.array(accompaniment_audio) * 32768).astype('int16')
 
         return [(sampling_rate, vocal_audio), (sampling_rate, accompaniment_audio)]
 
