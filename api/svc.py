29e415cb6adbf8e569cb57e7c87e83e595e6f667
Eric Liu <yuanliu325@gmail.com>
2023-03-23 00:39:55 +0800

    fix str converted to float; add exception on batch api

--- api/svc.py
+++ api/svc.py
@@ -153,6 +153,10 @@ class SingleInferenceHandler(api.base.ApiHandler):
             logger.debug(f"read file {audiofile_name}\n"
                          f"sampling rate: {sampling_rate}")
 
+            tran = float(tran)
+            th = float(th)
+            ns = float(ns)
+
             output_audio_array = _svc.inference(srcaudio=scraudio,
                                                 chara=dsid,
                                                 tran=tran,
@@ -189,78 +193,92 @@ class SingleInferenceHandler(api.base.ApiHandler):
 # noinspection PyAbstractClass
 class BatchInferenceHandler(api.base.ApiHandler):
     async def post(self):
-        from zipfile import ZipFile
-        from scipy.io import wavfile
-        import uuid
+        try:
+            from zipfile import ZipFile
+            from scipy.io import wavfile
+            import uuid
 
-        dsid = self.get_argument("dsid", "")
-        tran = self.get_argument("tran", "0")
-        th = self.get_argument("th", "-40.0")
-        ns = self.get_argument("ns", "0.4")
-        audiofile_dict = self.request.files.get("srcaudio", [])
+            dsid = self.get_argument("dsid", "")
+            tran = self.get_argument("tran", "0")
+            th = self.get_argument("th", "-40.0")
+            ns = self.get_argument("ns", "0.4")
+            audiofile_dict = self.request.files.get("srcaudio", [])
 
-        if not audiofile_dict:
-            self.set_status(400)
+            if not audiofile_dict:
+                self.set_status(400)
+                self.write({
+                    "code": 400,
+                    "msg": "未上传文件！",
+                    "data": None
+                })
+                return
+
+            if dsid == "":
+                self.set_status(400)
+                self.write({
+                    "code": 400,
+                    "msg": "未选择模型！",
+                    "data": None
+                })
+                return
+
+            temp_dir_name = "temp"
+
+            # get the path to the parent directory
+            parent_dir = os.path.abspath(os.path.join(os.getcwd(), os.curdir))
+
+            # construct the path to the "temp" directory
+            temp_dir = os.path.join(parent_dir, temp_dir_name)
+
+            logger.debug(f"TempDir: {temp_dir}")
+
+            if not os.path.exists(temp_dir):
+                os.mkdir(temp_dir)
+
+            tmp_workdir_name = f"{temp_dir}/batch_{uuid.uuid4()}"
+            if not os.path.exists(tmp_workdir_name):
+                os.mkdir(tmp_workdir_name)
+
+            output_files = []
+
+            tran = float(tran)
+            th = float(th)
+            ns = float(ns)
+
+            for idx, file in enumerate(audiofile_dict):
+                audio_filename = file["filename"]
+                audio_filebody = file["body"]
+                filename = os.path.basename(audio_filename)
+
+                print(f"{idx}, {file}, {filename}")
+                sampling_rate, audio = wavfile.read(io.BytesIO(audio_filebody))
+                output_sampling_rate, output_audio = _svc.inference((sampling_rate, audio), chara=dsid, tran=tran,
+                                                                    slice_db=th, ns=ns)
+                new_filepath = f"{tmp_workdir_name}/{filename}"
+                wavfile.write(filename=new_filepath, rate=output_sampling_rate, data=output_audio)
+                output_files.append(new_filepath)
+
+            zipfilename = f"{tmp_workdir_name}/output.zip"
+            with ZipFile(zipfilename, "w") as zip_obj:
+                for idx, filepath in enumerate(output_files):
+                    zip_obj.write(filepath, os.path.basename(filepath))
+
+            # todo: remove data
+
+            # set response header and body
+            self.set_header("Content-Type", "application/zip")
+            self.set_header("Content-Disposition", "attachment; filename=output.zip")
+            with open("output.zip", "rb") as file:
+                self.write(file.read())
+        except Exception as e:
+            logger.exception(e)
+            self.set_status(500)
             self.write({
-                "code": 400,
-                "msg": "未上传文件！",
+                "code": 500,
+                "msg": "system_error",
                 "data": None
             })
             return
-
-        if dsid == "":
-            self.set_status(400)
-            self.write({
-                "code": 400,
-                "msg": "未选择模型！",
-                "data": None
-            })
-            return
-
-        temp_dir_name = "temp"
-
-        # get the path to the parent directory
-        parent_dir = os.path.abspath(os.path.join(os.getcwd(), os.curdir))
-
-        # construct the path to the "temp" directory
-        temp_dir = os.path.join(parent_dir, temp_dir_name)
-
-        logger.debug(f"TempDir: {temp_dir}")
-
-        if not os.path.exists(temp_dir):
-            os.mkdir(temp_dir)
-
-        tmp_workdir_name = f"{temp_dir}/batch_{uuid.uuid4()}"
-        if not os.path.exists(tmp_workdir_name):
-            os.mkdir(tmp_workdir_name)
-
-        output_files = []
-
-        for idx, file in enumerate(audiofile_dict):
-            audio_filename = file["filename"]
-            audio_filebody = file["body"]
-            filename = os.path.basename(audio_filename)
-
-            print(f"{idx}, {file}, {filename}")
-            sampling_rate, audio = wavfile.read(io.BytesIO(audio_filebody))
-            output_sampling_rate, output_audio = _svc.inference((sampling_rate, audio), chara=dsid, tran=tran,
-                                                                slice_db=th, ns=ns)
-            new_filepath = f"{tmp_workdir_name}/{filename}"
-            wavfile.write(filename=new_filepath, rate=output_sampling_rate, data=output_audio)
-            output_files.append(new_filepath)
-
-        zipfilename = f"{tmp_workdir_name}/output.zip"
-        with ZipFile(zipfilename, "w") as zip_obj:
-            for idx, filepath in enumerate(output_files):
-                zip_obj.write(filepath, os.path.basename(filepath))
-
-        # todo: remove data
-
-        # set response header and body
-        self.set_header("Content-Type", "application/zip")
-        self.set_header("Content-Disposition", "attachment; filename=output.zip")
-        with open("output.zip", "rb") as file:
-            self.write(file.read())
 
 
 if __name__ == "__main__":
