98917165bf78afb92e667d43b7140795cb18d551
Erikk0 <yuanliu325@gmail.com>
2023-03-26 05:23:12 +0800

    change VM output to zip fil

--- api/vm.py
+++ api/vm.py
@@ -1,6 +1,9 @@
 import io
 import logging
+import os
 import wave
+import uuid
+from zipfile import ZipFile
 
 import numpy as np
 from scipy.io import wavfile
@@ -31,14 +34,40 @@ class VocalRemoverHandler(api.base.ApiHandler):
             v = VEX()
             [(vocal_sampling_rate, vocal_audio), (accompaniment_sampling_rate, accompaniment_audio)] = v.separate((sampling_rate, audio_array))
 
-            self.set_header('Content-Type', 'audio/wav')
-            self.set_header('Content-Disposition', 'attachment; filename="vocals.wav"')
-            wavfile.write(self, vocal_sampling_rate, vocal_audio)
-            self.write('\n')
-            self.set_header('Content-Type', 'audio/wav')
-            self.set_header('Content-Disposition', 'attachment; filename="accompaniment.wav"')
-            wavfile.write(self, accompaniment_sampling_rate, accompaniment_audio)
+            output_dirname = f"{uuid.uuid4()}"
+            output_dir = f"output/{output_dirname}"
+            if not os.path.exists(output_dir):
+                os.mkdir(output_dir)
+            wavfile.write(f"{output_dir}/vocals.wav", vocal_sampling_rate, vocal_audio)
+            wavfile.write(f"{output_dir}/accompaniment.wav", accompaniment_sampling_rate, accompaniment_audio)
+
+            zipfilename = f"{output_dir}/output.zip"
+            with ZipFile(zipfilename, 'w') as zip_obj:
+                zip_obj.write(f"{output_dir}/vocals.wav", "vocals.wav")
+                zip_obj.write(f"{output_dir}/accompaniment.wav", "accompaniment.wav")
+
+            logger.debug(f"start output data.")
+            # set response header and body
+            self.set_header("Content-Type", "application/zip")
+            self.set_header("Content-Disposition", "attachment; filename=output.zip")
+            with open(zipfilename, "rb") as file:
+                self.write(file.read())
             await self.flush()
+            logger.debug(f"response completed.")
+
+            os.remove(f"{output_dir}/vocals.wav")
+            os.remove(f"{output_dir}/accompaniment.wav")
+            os.remove(f"{output_dir}/output.zip")
+            os.rmdir(output_dir)
+
+            # self.set_header('Content-Type', 'audio/wav')
+            # self.set_header('Content-Disposition', 'attachment; filename="vocals.wav"')
+            # wavfile.write(self, vocal_sampling_rate, vocal_audio)
+            # self.write('\n')
+            # self.set_header('Content-Type', 'audio/wav')
+            # self.set_header('Content-Disposition', 'attachment; filename="accompaniment.wav"')
+            # wavfile.write(self, accompaniment_sampling_rate, accompaniment_audio)
+            # await self.flush()
         except Exception as e:
             logger.exception(e)
             self.set_status(500)
--- vextract/vocal_extract.py
+++ vextract/vocal_extract.py
@@ -43,36 +43,42 @@ class VEX(object):
             temp_filename
         ])
 
+        temp_dir = f"output/{temp_filebasename}"
         vocal_file = f"output/{temp_filebasename}/vocals.wav"
         accompaniment_file = f"output/{temp_filebasename}/accompaniment.wav"
 
-        with wave.open(vocal_file, 'rb') as wav_file:
-            num_frames = wav_file.getnframes()
-            audiofile_body = wav_file.readframes(num_frames)
-        with io.BytesIO(audiofile_body) as file_stream:
-            with wave.open(file_stream, 'rb') as wave_file:
-                audio_data = wave_file.readframes(-1)
-                vocal_sampling_rate = wave_file.getframerate()
-                num_channels = wave_file.getnchannels()
+        vocal_sampling_rate, vocal_audio = wavfile.read(vocal_file)
 
-        vocal_audio = np.frombuffer(audio_data, dtype=np.int16)
-        vocal_audio = np.reshape(vocal_audio, (-1, num_channels))
+        # with wave.open(vocal_file, 'rb') as wav_file:
+        #     num_frames = wav_file.getnframes()
+        #     audiofile_body = wav_file.readframes(num_frames)
+        # with io.BytesIO(audiofile_body) as file_stream:
+        #     with wave.open(file_stream, 'rb') as wave_file:
+        #         audio_data = wave_file.readframes(-1)
+        #         vocal_sampling_rate = wave_file.getframerate()
+        #         num_channels = wave_file.getnchannels()
+        #
+        # vocal_audio = np.frombuffer(audio_data, dtype=np.int16)
+        # vocal_audio = np.reshape(vocal_audio, (-1, num_channels))
 
-        with wave.open(accompaniment_file, 'rb') as wav_file:
-            num_frames = wav_file.getnframes()
-            audiofile_body = wav_file.readframes(num_frames)
-        with io.BytesIO(audiofile_body) as file_stream:
-            with wave.open(file_stream, 'rb') as wave_file:
-                audio_data = wave_file.readframes(-1)
-                accompaniment_sampling_rate = wave_file.getframerate()
-                num_channels = wave_file.getnchannels()
+        accompaniment_sampling_rate, accompaniment_audio = wavfile.read(accompaniment_file)
 
-        accompaniment_audio = np.frombuffer(audio_data, dtype=np.int16)
-        accompaniment_audio = np.reshape(accompaniment_audio, (-1, num_channels))
+        # with wave.open(accompaniment_file, 'rb') as wav_file:
+        #     num_frames = wav_file.getnframes()
+        #     audiofile_body = wav_file.readframes(num_frames)
+        # with io.BytesIO(audiofile_body) as file_stream:
+        #     with wave.open(file_stream, 'rb') as wave_file:
+        #         audio_data = wave_file.readframes(-1)
+        #         accompaniment_sampling_rate = wave_file.getframerate()
+        #         num_channels = wave_file.getnchannels()
+        #
+        # accompaniment_audio = np.frombuffer(audio_data, dtype=np.int16)
+        # accompaniment_audio = np.reshape(accompaniment_audio, (-1, num_channels))
 
         os.remove(temp_filename)
         os.remove(vocal_file)
         os.remove(accompaniment_file)
+        os.rmdir(temp_dir)
 
         return [(vocal_sampling_rate, vocal_audio), (accompaniment_sampling_rate, accompaniment_audio)]
 
